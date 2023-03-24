2015416e53b55059cb420995ebe1c2465e09bc3f
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2023-03-24 17:59:24 +0100

    Empezando hacer mapping

--- src/controladores/c_habitacion.js
+++ src/controladores/c_habitacion.js
@@ -1,78 +1,74 @@
 const pool = require('../database')
 
-module.exports = {
-    leerHabitaciones : async(req,res)=>{
-        try {
-            const habitaciones = await pool.query('Select * from habitacion')
-            res.json(habitaciones)
-        } catch (error) {
-            res.json({
-                error:error,
-                mensaje:"No se ha podido leer las habitaciones"
-            })
-        }
-    },
-    leerHabitacionId: async(req,res)=>{
-        try {
-            const id = req.params.id
-            const habitacion = await pool.query('Select * from habitacion where id = ?', [id])
-            res.json(habitacion)
-        } catch (error) {
-            res.json({
-                error:error,
-                mensaje:"No se ha podido leer la habitación"
-            })
-        } 
-    },
-    insertarHabitacion: async(req,res)=>{
-        try {
-            const {cama, escritorio, armario, precio, cfPiso } = req.body
-            const nuevaHabitacion = {
-                cama,
-                escritorio,
-                armario,
-                precio,
-                cfPiso
-            }
-            await pool.query('insert into habitacion set ?',[nuevaHabitacion])
-            res.json('Se ha creado correctamente')
-        } catch (error) {
-            res.json({
-                error:error,
-                mensaje:"No se ha podido crear la habitación"
-            })
-        } 
-    },
-    actualizarHabitacion: async(req,res)=>{
-        try {
-            const id = req.params.id
-            const {cama, escritorio, armario, precio, cfPiso } = req.body
-            const nuevaHabitacion = {
-                cama,
-                escritorio,
-                armario,
-                precio,
-                cfPiso
-            }
-            await pool.query('Update habitacion set ? where id = ?', [nuevaHabitacion, id])
-            res.json('Se ha actualizado correctamente')
-        } catch (error) {
-            res.json({
-                error:error,
-                mensaje:"No se ha podido actualizar la habitación"
-            })
-        } 
-    },
-    eliminarHabitacion: async(req,res)=>{
-        try {
-            const { id } = req.params 
-            await pool.query('DELETE FROM habitacion WHERE ID = ?', [id])
-            res.json('Se ha eliminado correctamente')
-        } catch (error) {
-            res.json({
-                error:error,
-                mensaje:"No se ha podido eliminar la habitación"
-            })
-        } 
+module.exports = class Habitacion {
+    // Mapping de propiedades de la tabla perfiles
+    constructor(id=null, cama=null, escritorio=null, armario=null, precio=null, cfPiso=null) {
+      this.id = id
+      this.cama = cama
+      this.escritorio = escritorio
+      this.armario = armario
+      this.precio = precio
+      this.cfPiso = cfPiso
     }
+  
+    //leer todos
+    static async getAll() {
+        console.log("hola")
+        const habitaciones = await pool.query('Select * from habitacion')
+
+        const habitacionesMap = habitaciones.map(({ id, cama, escritorio,armario,precio,cfPiso }) => {
+            return new Habitacion(id, cama, escritorio,armario,precio,cfPiso);
+        });      
+
+        return habitacionesMap
+    }
+
+    static async getAll() {
+        const habitaciones = await pool.query('Select * from habitacion')
+
+        const habitacionesMap = habitaciones.map(({ id, cama, escritorio,armario,precio,cfPiso }) => {
+            return new Habitacion(id, cama, escritorio,armario,precio,cfPiso);
+        });      
+
+        return habitacionesMap
+    }
+
+    static async getById(id) {
+        // Consultar a la base de datos para obtener la habitación con el ID especificado
+        const query = 'SELECT * FROM habitacion WHERE id = ?';
+        const resultados = await pool.query(query, [id]);
+    
+        // Si no se encuentra ninguna habitación con ese ID, devolver null
+        if (resultados.length === 0) {
+          return null;
+        }
+
+        console.log(resultados)
+    
+        const habitacion = resultados[0];
+        // Crear un objeto Habitacion a partir de los resultados y devolverlo
+        return new Habitacion
+        (habitacion.id, habitacion.cama, habitacion.escritorio, habitacion.armario, habitacion.precio, habitacion.cfPiso)
+    }
+
+    static async create(nuevaHabitacion) {
+        // Insertar una nueva habitación en la base de datos
+        await pool.query('insert into habitacion set ?',[nuevaHabitacion])
+    
+        // Crear un nuevo objeto Habitacion a partir de los resultados y devolverlo
+        return('Se ha creado correctamente')
+    }
+    
+    static async update() {
+        // Actualizar la habitación en la base de datos
+        const query = 'UPDATE habitaciones SET numero = ?, tipo = ? WHERE id = ?';
+        await pool.query(query, [this.numero, this.tipo, this.id]);
+    
+    }
+    
+    
 }
+
+  
+  
+
--- src/routes/r_habitacion.js
+++ src/routes/r_habitacion.js
@@ -1,17 +1,64 @@
 const express = require('express')
-const { leerHabitaciones, leerHabitacionId, insertarHabitacion, eliminarHabitacion, actualizarHabitacion } = require('../controladores/c_habitacion')
+const Habitacion = require('../controladores/c_habitacion')
+
 const r_habitacion = express.Router()
 //base de datos pero el le llama pool
 
 
-r_habitacion.get('/', leerHabitaciones)
+r_habitacion.get('/',async (req, res) => {
+    try {
+        const habitaciones = await Habitacion.getAll();
+        res.send(habitaciones);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-r_habitacion.get('/:id',leerHabitacionId)
+r_habitacion.get('/:id',async (req, res) => {
+    try {
+        const id = req.params.id
+        const habitacionId = await Habitacion.getById(id);
+        res.send(habitacionId);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-r_habitacion.post('/',insertarHabitacion)
+r_habitacion.post('/',async (req, res) => {
+    try {
+        const {cama, escritorio, armario, precio, cfPiso } = req.body
+        const nuevaHabitacion = {
+            cama,
+            escritorio,
+            armario,
+            precio,
+            cfPiso
+        }
+        const respuesta = await Habitacion.create(nuevaHabitacion);
+        res.json(respuesta)
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-r_habitacion.delete('/:id',eliminarHabitacion)
+// r_habitacion.delete('/:id',eliminarHabitacion)
 
-r_habitacion.put('/:id',actualizarHabitacion)
+r_habitacion.put('/:id',async (req, res) => {
+    try {
+        const id = req.params.id
+        const {cama, escritorio, armario, precio, cfPiso } = req.body
+        const nuevaHabitacion = {
+            cama,
+            escritorio,
+            armario,
+            precio,
+            cfPiso
+        }
+        const respuesta = await Habitacion.create(nuevaHabitacion);
+        res.json(respuesta)
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 module.exports = r_habitacion
