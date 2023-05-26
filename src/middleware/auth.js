6feec3a0dda6c32c002e87490d4e937911f7f0fc
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2023-05-26 15:56:27 +0200

    prueba

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,16 +5,17 @@ const auth = async (req, res, next)=>{
     //capturamos el token que envian en la cabecera
     if(req.headers.authorization){
         const token = req.headers.authorization.split(' ')[1]
-        console.log(token)
         //desencriptamos el token
         try {
             const tokenVerificado = jwt.verify(token, process.env.secret)
-
+            console.log(tokenVerificado)
             //buscamos en base de datos el usario con el id
             const usuarioEncontrado = await Cliente.getById(tokenVerificado.data.userID)
             
             req.query.userID = tokenVerificado.data.userID
+            console.log('userID: ' + tokenVerificado.data.userID)
             req.query.roles = tokenVerificado.data.roles
+            console.log('Roles: ' + tokenVerificado.data.roles)
             //Pasamos al siguiente middleware (en este caso el controlador c_mascotas)
             next()
 
